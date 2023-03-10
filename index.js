d3a9829c0e0d3bf5f6574f62a3f3590f7051ec44
JacquesVida <jacques_vida01@student.csulb.edu>
2023-03-09 21:40:44 -0800

    v1

---
+++ index.js
@@ -0,0 +1,12 @@
+// Add required packages
+
+const express = require("express");
+const app = express();
+
+const path = require("path");
+
+
+//const multer = require("multer");
+//const upload = multer();
+
+//require('dotenv').config();
