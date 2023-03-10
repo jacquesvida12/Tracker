394029ab5d13a828f29f2332d466c68b9c7e6ca7
JacquesVida <jacques_vida01@student.csulb.edu>
2023-03-09 23:42:37 -0800

    Database created - create next layers tomorrow

---
+++ calorie_table.sql
@@ -0,0 +1,24 @@
+-- DROP the table
+DROP TABLE CALORIES;
+
+--Create table
+CREATE TABLE CALORIES (
+    cal_id SERIAL PRIMARY KEY,
+    cal_name VARCHAR(50) NOT NULL,
+    cal_meal VARCHAR(9) NOT NULL,
+    cal_protien NUMERIC,
+    cal_carbs NUMERIC,
+    cal_fats NUMERIC,
+    cal_calories NUMERIC NOT NULL, 
+    cal_date DATE,
+    cal_time TIME,
+    cal_notes VARCHAR(60)
+);
+
+--inserting records for THU MARCH 9
+
+INSERT INTO CALORIES (cal_id,cal_name,cal_meal,cal_protien,cal_carbs,cal_fats,cal_calories,cal_date,cal_time,cal_notes)
+VALUES
+('1','WafflePB','Breakfast','11','45','17','370','3-9-2023','9:00',''),
+('2','Quesadilla','Lunch','0','0','0','600','3-9-2023','1:30',''),
+('3','Sausage and Potatoe','Dinner','0','0','0','450','3-9-2023','7:15','');
--- index.js
+++ index.js
@@ -1,12 +1,44 @@
-// Add required packages
-
+//Adding the required the packages
 const express = require("express");
 const app = express();
 
-const path = require("path");
+require('dotenv').config();
 
+// Add packages
+require("dotenv").config();
+// Add database package and connection string
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.CRUNCHY_DATABASE_URL,
+  ssl: {
+      rejectUnauthorized: false
+  }
+});
 
-//const multer = require("multer");
-//const upload = multer();
+//Set up EJS 
+app.set("view engine", "ejs");
 
-//require('dotenv').config();
+//Setup routes
+app.get("/", (req, res) => {
+    //res.send ("Hello world...");
+    const sql = "SELECT * FROM CALORIES ORDER BY CAL_ID";
+    pool.query(sql, [], (err, result) => {
+        var message = "";
+        var model = {};
+        if(err) {
+            message = `Error - ${err.message}`;
+        } else {
+            message = "success";
+            model = result.rows;
+        };
+        res.render("index", {
+            message: message,
+            model : model
+        });
+    });
+});
+
+//Start listener
+app.listen(process.env.PORT || 3000, () => {
+    console.log("The Server has now started at (http://localhost:3000/")
+});
