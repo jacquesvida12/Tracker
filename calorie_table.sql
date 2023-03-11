1b4ac70ffffb917a50eab87c50d8ccee58d030ea
jacques <jacques.vida01@student.csulb.edu>
2023-03-10 22:13:14 -0800

    V2 3/10/23

--- calorie_table.sql
+++ calorie_table.sql
@@ -6,7 +6,7 @@ CREATE TABLE CALORIES (
     cal_id SERIAL PRIMARY KEY,
     cal_name VARCHAR(50) NOT NULL,
     cal_meal VARCHAR(9) NOT NULL,
-    cal_protien NUMERIC,
+    cal_protein NUMERIC,
     cal_carbs NUMERIC,
     cal_fats NUMERIC,
     cal_calories NUMERIC NOT NULL, 
@@ -17,8 +17,9 @@ CREATE TABLE CALORIES (
 
 --inserting records for THU MARCH 9
 
-INSERT INTO CALORIES (cal_id,cal_name,cal_meal,cal_protien,cal_carbs,cal_fats,cal_calories,cal_date,cal_time,cal_notes)
+INSERT INTO CALORIES (cal_id,cal_name,cal_meal,cal_protein,cal_carbs,cal_fats,cal_calories,cal_date,cal_time,cal_notes)
 VALUES
-('1','WafflePB','Breakfast','11','45','17','370','3-9-2023','9:00',''),
-('2','Quesadilla','Lunch','0','0','0','600','3-9-2023','1:30',''),
-('3','Sausage and Potatoe','Dinner','0','0','0','450','3-9-2023','7:15','');
+('1','WafflePB','Breakfast','11','45','17','370','3/9/2023','9:00',''),
+('2','Quesadilla','Lunch','0','0','0','600','3/9/2023','1:30',''),
+('3','Sausage and Potatoe','Dinner','0','0','0','450','3/9/2023','7:15',''),
+('4','WafflePB','Breakfast','11','45','17','370','3/9/2023','9:00','');
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 
 require('dotenv').config();
+const path = require('path');
 
 // Add packages
 require("dotenv").config();
@@ -15,8 +16,11 @@ const pool = new Pool({
   }
 });
 
-//Set up EJS 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false })); // <--- middleware configuration
+
 
 //Setup routes
 app.get("/", (req, res) => {
@@ -37,6 +41,69 @@ app.get("/", (req, res) => {
         });
     });
 });
+
+//=====================================================================
+app.get("/manage", (req, res) => {
+    //res.send ("Hello world...");
+    const sql = "SELECT * FROM CALORIES ORDER BY CAL_ID";
+    pool.query(sql, [], (err, result) => {
+        var message = "";
+        var model = {};
+        if(err) {
+            message = `Error - ${err.message}`;
+        } else {
+            message = "success";
+            model = result.rows;
+        };
+        res.render("manage", {
+            message: message,
+            model : model
+        });
+    });
+});
+
+//=============================================================================================
+
+// GET /edit/5
+app.get("/edit/:id", (req, res) => {
+    const id = req.params.id;
+    const sql = "SELECT * FROM CALORIES WHERE cal_id = $1";
+    pool.query(sql, [id], (err, result) => {
+      // if (err) ...
+      res.render("edit", { model: result.rows[0] });
+    });
+  });
+
+  // POST /edit/5
+app.post("/edit/:id", (req, res) => {
+    const id = req.params.id;
+    const meal = [req.body.Name, req.body.Meal, req.body.Protein, req.body.Carbs, req.body.Fats, req.body.Calories, req.body.Date, req.body.Time, req.body.Notes,id];
+    const sql = "UPDATE CALORIES SET cal_name = $1, cal_meal = $2, cal_protein = $3, cal_carbs = $4, cal_fats = $5, cal_calories = $6, cal_date = $7, cal_time = $8, cal_notes = $9 WHERE (cal_id = $10)";
+    pool.query(sql, meal, (err, result) => {
+      // if (err) ...
+      if (err)
+      console.log(err);
+      res.redirect("/");
+    });
+  });
+
+
+
+app.get("/create", (req, res) => {
+    const id = req.params.id;
+      res.render("create", { model:{} });
+    });
+// POST /create
+app.post("/create", (req, res) => {
+    const sql = "INSERT INTO CALORIES (cal_name, cal_meal, cal_protein, cal_carbs, cal_fats,cal_calories, cal_date, cal_time, cal_notes) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)";
+    const meal = [req.body.Name, req.body.Meal, req.body.Protein, req.body.Carbs, req.body.Fats, req.body.Calories, req.body.Date, req.body.Time, req.body.Notes];
+    pool.query(sql, meal, (err, result) => {
+      // if (err) ...
+      if (err)
+      console.log(err)
+      res.redirect("/");
+    });
+  });
 
 //Start listener
 app.listen(process.env.PORT || 3000, () => {
